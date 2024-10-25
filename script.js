598fe48c93afad3fcce638d22f7e7f9fad5209a3
Mohamed Abdelhafiz <mohamed.abdelhafiz.dev@gmail.com>
2024-10-25 03:02:03 +0300

    creating remaining files

---
+++ script.js
@@ -0,0 +1,16 @@
+let scrollToBottomBtn = document.querySelector(".scroll-btn button");
+let skillsNav = document.querySelector("#skills-nav");
+scrollToBottomBtn.addEventListener("click", function (event) {
+  window.scrollTo(0, 500);
+  this.style.display = "none";
+});
+skillsNav.addEventListener("click", function (event) {
+  window.scrollTo(0, 500);
+});
+window.onscroll = function (event) {
+  if (window.scrollY <= 300) {
+    scrollToBottomBtn.style.display = "inline";
+  } else {
+    scrollToBottomBtn.style.display = "none";
+  }
+};
